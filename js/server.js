d93a23149e4643c62b5a62f81d812e23b36faf0c
Gaston Michelan <gaston.michelan@fi.uncoma.edu.ar>
2017-11-17 14:21:47 -0300

    agregados cambios al servidor

--- js/server.js
+++ js/server.js
@@ -14,14 +14,18 @@ app.get('/inicio',function(req,res){
     console.log(mensaje);
 	var ar = logica.iniciarJugador(jug, eq, rol);
 	res.send(ar[0] + ' ' + ar[1] + ' ' + ar[2]);
+	console.log("Jugador creado salida del método inicio");
 
 });
 
 app.post('/mover',function(req,res){
+	
 	var tok = req.param('token');
 	var num = req.param('num');
-	msg = logica.mover(token, num);
+	msg = logica.mover(tok, num);
+	console.log("Se movio el jugador: "+tok+" en la posición"+num);
 	res.send(msg);
+	console.log("terminado el método mover");
 	
 });
 
@@ -37,9 +41,10 @@ function jugador(token) {
 }
 
 app.post('/atacar',function(req,res){
+	console.log("Entro al método atacar");
 	if (seEncuentra(req.param('token')))
 	{
-    res.send('el jugador '+jugador(token)+' ataca!');}
+    res.send('el jugador '+jugador(token)+' ataca!');}//hay que revisar el token que recibe este método
 	else{
 		res.send('El token no existe perrito, deja de paquearte jodi2');
 	}
